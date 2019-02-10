bab2ad8f5c8e794c0097b67dd24bd83464779bae
Gaurav Kaushik <vgauravk@gmail.com>
2019-02-10 21:23:01 +0530

    In Singly Linked List Implmentation, added few docs

--- LinkedList/SinglyLinkedList.py
+++ LinkedList/SinglyLinkedList.py
@@ -6,6 +6,22 @@ Remember, in a singly linked list, we have an ordered list of items as individua
 Nodes that have pointers to other Nodes.
 """
 
+# In a Linked List the first node is called the head and the last node is called the
+# tail. Let's discuss the pros and cons of Linked Lists:
+#
+# Pros
+# Linked Lists have constant-time insertions and deletions in any position, in
+# comparison, arrays require O(n) time to do the same thing.
+#
+# Linked lists can continue to expand without having to specify their size ahead of
+# time
+#
+# Cons
+# To access an element in a linked list, you need to take O(k) time to go from the head
+# of the list to the kth element. In contrast, arrays have constant time operations to
+# access elements in an array
+
+
 class Node:
 
     def __init__(self, value):
